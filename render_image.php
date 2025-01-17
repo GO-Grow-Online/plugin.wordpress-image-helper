4c0a3d59f31d722469cefa79a8e560e53fcd7104
GO-Grow-Online <julien.growonline@gmail.com>
2025-01-17 16:55:12 +0100

    tweak

--- render_image.php
+++ render_image.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Wordpress Image Renderer
  * Description: A plugin to render images based on a Twig-like template logic.
- * Version: 1.0.10
+ * Version: 1.0.11
  * Author: Grow Online
  */
 
