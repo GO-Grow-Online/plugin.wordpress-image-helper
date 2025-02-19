af0455e133400983ab7965f1570831ba0b315ec7
Grow Online <139146912+GO-Grow-Online@users.noreply.github.com>
2025-02-19 09:17:54 +0100

    tweaks

--- wp_medias_settings.php
+++ wp_medias_settings.php
@@ -15,7 +15,7 @@ add_filter('intermediate_image_sizes_advanced', 'remove_unwanted_image_sizes');
 
 // Display svg's code instead of an 'img' element
 if (!function_exists('get_svg')) {
-    function get_svg( $media_file, $is_url ) {
+    function get_svg( $media_file, $is_url = true ) {
         if ($is_url) {
             if ($media_file['mime_type'] === 'image/svg+xml') {
                 $file_path = get_attached_file( $media_file['ID'] );
