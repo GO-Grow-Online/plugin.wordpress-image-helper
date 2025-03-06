99af1c7f6ee7a07dd49a030430f1089da9c7ef29
Grow Online <139146912+GO-Grow-Online@users.noreply.github.com>
2025-03-06 13:13:34 +0100

    tweak

--- wp_pages.php
+++ wp_pages.php
@@ -1,7 +1,7 @@
 <?php
 
 add_filter('plugin_action_links_' . plugin_basename(__FILE__), function ($links) {
-    $settings_link = '<a href="admin.php?page=mon-plugin-license">' . __('Configuration') . '</a>';
+    $settings_link = '<a href="admin.php?page=go-image-renderer-license">' . __('Configuration') . '</a>';
     array_unshift($links, $settings_link); // Ajoute le lien en premier
     return $links;
 });
@@ -9,25 +9,25 @@ add_filter('plugin_action_links_' . plugin_basename(__FILE__), function ($links)
 // Ajouter la page "Configuration" sous "Réglages"
 add_action('admin_menu', function () {
     add_menu_page(
-        'Configuration du Plugin',
-        'Mon Plugin',
+        'Configuration de Image Renderer',
+        'GO - Image Renderer',
         'manage_options',
-        'mon-plugin-license',
-        'mon_plugin_license_page',
+        'go-image-renderer-license',
+        'go_image_renderer_license_page',
         'dashicons-admin-generic', // Icône du menu
         80 // Position dans le menu
     );
 });
 
 // Affichage de la page "Configuration"
-function mon_plugin_license_page() {
+function go_image_renderer_license_page() {
     ?>
     <div class="wrap">
         <h1>Activation du Plugin</h1>
         <form method="post" action="options.php">
             <?php
-            settings_fields('mon_plugin_licence_group');
-            do_settings_sections('mon-plugin-license');
+            settings_fields('go_image_renderer_licence_group');
+            do_settings_sections('go-image-renderer-license');
             submit_button();
             ?>
         </form>
@@ -37,23 +37,23 @@ function mon_plugin_license_page() {
 
 // Ajouter les champs d'activation de la licence
 add_action('admin_init', function () {
-    register_setting('mon_plugin_licence_group', 'mon_plugin_license_key');
+    register_setting('go_image_renderer_licence_group', 'go_image_renderer_license_key');
 
     add_settings_section(
-        'mon_plugin_licence_section',
+        'go_image_renderer_licence_section',
         'Entrez votre clé de licence',
         null,
-        'mon-plugin-license'
+        'go-image-renderer-license'
     );
 
     add_settings_field(
-        'mon_plugin_license_key',
+        'go_image_renderer_license_key',
         'Clé de licence',
         function () {
-            $value = get_option('mon_plugin_license_key', '');
-            echo '<input type="text" name="mon_plugin_license_key" value="' . esc_attr($value) . '" class="regular-text">';
+            $value = get_option('go_image_renderer_license_key', '');
+            echo '<input type="text" name="go_image_renderer_license_key" value="' . esc_attr($value) . '" class="regular-text">';
         },
-        'mon-plugin-license',
-        'mon_plugin_licence_section'
+        'go-image-renderer-license',
+        'go_image_renderer_licence_section'
     );
 });
