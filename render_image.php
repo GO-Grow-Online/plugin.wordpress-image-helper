95b416e408bebbc8bb9cc555433d3ea5bb72ac7c
Grow Online <139146912+GO-Grow-Online@users.noreply.github.com>
2025-09-03 10:29:36 +0200

    css tweak

--- render_image.php
+++ render_image.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: GO - Media Renderer
  * Description: Display images & videos with render_image() & render_videos(), powerfull and light functions that brings performance and accessibility to your theme. 
- * Version: 2.0.2.3
+ * Version: 2.0.2.4
  * Author: Grow Online
  */
 
